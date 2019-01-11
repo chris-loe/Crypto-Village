7c35078912923b5c6c895cbcb9f564dab59ab340
chris-loe <loebaorong111@gmail.com>
2019-01-11 14:09:18 +0800

    heroku

--- cryptosite/settings.py
+++ cryptosite/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "crypto/static"),
+    os.path.join(BASE_DIR, "static"),
     #'/Buckboo/blog_app/static/',
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressManifestStaticFilesStorage'
